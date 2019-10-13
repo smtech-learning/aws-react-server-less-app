306afc7dc16362661cfd02a6c6f7908770e93a03
agtechworld <sureshmunnangi@gmail.com>
2019-10-12 21:36:20 -0400

    commit

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import { Auth } from 'aws-amplify';
 import registration from '../Images/registration.png';
 
+
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (valid = false)
+  );
+  return valid;
+}
+
+const countErrors = (errors) => {
+  let count = 0;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (count = count+1)
+  );
+  return count;
+}
 class REgister1 extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             username: '',
             password: '',
@@ -13,29 +28,78 @@ class REgister1 extends Component {
             phone_numbe:'',
             confirmation_code: '',
             signedup: false,
-            confirmed: false
+            confirmed: false,
+            errors: {
+                fullName: '',
+                username: '',
+                email: '',
+                password: '',
+                phone_number:''
+            }
         }
-        this.state = { hasError: false };
-        this.state = { errorDescription: '' };
-        
-        this.handlechnage = this.handlechnage.bind(this);
         this.handlesubmit = this.handlesubmit.bind(this);
-    }
+    
 
-    handlesubmit(e) {
+      }
+    
+    
+    handleChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        let errors = this.state.errors;
+        const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+
+        switch (name) {
+          case 'username': 
+            errors.username = 
+            validEmailRegex.test(value)
+            ? ''
+            : 'Email is not valid!';
+            break;
+          case 'email': 
+            errors.email = 
+              validEmailRegex.test(value)
+                ? ''
+                : 'Email is not valid!';
+                break;
+            
+            case 'phone_number': 
+                errors.phone_number = 
+                  validEmailRegex.test(value)
+                    ? ''
+                    : 'Phone Number is not valid!';
+                break;
+          case 'password': 
+            errors.password = 
+              value.length < 8
+                ? 'Password must be 8 characters long!'
+                : '';
+            break;
+          default: console.log('testing here');
+            break;
+        }
+    
+        this.setState({errors, [name]: value});
+      }
+    
+      handleSubmit1 = (event) => {
+        event.preventDefault();
+        this.setState({formValid: validateForm(this.state.errors)});
+        this.setState({errorCount: countErrors(this.state.errors)});
+      }
+    
+      handlesubmit(e) {
         e.preventDefault();
-        e.target.reset();
         const { username, password, email, phone_number ,signedup,confirmed} = this.state;
         if (!signedup && !confirmed) {
             Auth.signUp({
                 username,
                 password,
                 attributes: {
-                    email,             // optional
-                    phone_number,      // optional - E.164 number convention
-                    // Other custom attributes...
+                    email,            
+                    phone_number,      
                 },
-                validationData: [],  // optional
+                validationData: [],  
             })
                 .then(data => {
                     console.log(data);
@@ -64,69 +128,83 @@ class REgister1 extends Component {
         }
     }
 
-    handlechnage(e) {
-        if (e.target.name === 'username') {
-            this.setState({ username: e.target.value })
-        } else if (e.target.name === 'password') {
-            this.setState({ password: e.target.value })
-            
-        } else if (e.target.name === 'email') {
-            this.setState({ email: e.target.value })
-        } else if (e.target.name === 'phone_number') {
-            this.setState({ phone_number: e.target.value })
-        } else if (e.target.name === 'confirmation_code') {
-            this.setState({ confirmation_code: e.target.value })
-        }
-    }
-
-    render() {
+      render() {
         if (!this.state.signedup) {
-            return (
-                <div className="register-container">
-                    <img src={registration} height="250px" />
-                    <form onSubmit={this.handlesubmit} class="form-container" autocomplete="off">
+            const {errors} = this.state;
+    
+                return (
+                    <div className="register-container">
+                        <img src={registration} height="250px" />
+                        
+                        <form onSubmit={this.handlesubmit} class="form-container" autoComplete="off">
                         <input id="_suburb" type="text" style={{ display: 'none' }} disabled />
                         
-                        {this.state.hasError && <h5 className="errorStyle"> {this.state.errorDescription}</h5>}
-                        
-                        <input type="text"  autocomplete="off_randomstring" className="login-style"  name="username" placeholder="Enter e-mail address" onChange={this.handlechnage} />
-                        <input type="text" autocomplete="off_randomstring" className="login-style"  name="email" placeholder="Renter the e-mail address" onChange={this.handlechnage} />
-                        
-                        <input type="password" autocomplete="new-password" className="login-style"   name="password" placeholder="Enter Password" onChange={this.handlechnage} />
-                        <input type="text" autocomplete="off_randomstring" className="login-style"   name="phone_number" placeholder="Enter Phone Number (Format should be: +1XXXXXXXXXX)" onChange={this.handlechnage} />
-                        <br />
-                        <button className="btn btn-primary"> Submit  </button>
-                    </form>
-                </div>
-            )
-        } else if (this.state.signedup && !this.state.confirmed) {
-            return (
-                <div className="register-container">
-                    <img src={registration} height="250px" />
-                    <form onSubmit={this.handlesubmit} class="form-container">
-                        <h5> You should have received the verification code in the e-mail.
-                        Plese check the e-mail and enter the verification code below and click Submit !</h5>
-                        {this.state.hasError && <h5 className="errorStyle"> {this.state.errorDescription}</h5>}
-
-                        <input type="text" name="confirmation_code" className="login-style" placeholder="Enter Verification Code" onChange={this.handlechnage} />
-                        <br />
-                        <button className="btn btn-primary"> Submit  </button>
-                    </form>
-                </div>
-            )
-        } else if (this.state.signedup && this.state.confirmed) {
-            return (
-                <div className="register-container">
-                    <img src={registration} height="250px" />
-                    <div className="form-container"> 
-                        <h3> Congratulations !! Registration successfully completed. </h3>
-                        <br />
-                        <button className="btn btn-primary"> Go to Login Page  </button>
+                            {this.state.hasError && <h5 className="errorStyle"> {this.state.errorDescription}</h5>}
+                            
+                            <input type="text" autocomplete="off_randomstring"
+                                className="login-style" name="username" placeholder="Enter e-mail address" 
+                                onChange={this.handleChange} />
+                            
+                              
+                            
+                                {errors.username.length > 0 && 
+                                    <span className='error'>{errors.username}</span>}
+                            
+                            <input type="text" autocomplete="off_randomstring"
+                                className="login-style" name="email" placeholder="Renter the e-mail address"
+                                onChange={this.handleChange} />
+                            
+                                {errors.email.length > 0 && 
+                                    <span className='error'>{errors.email}</span>}
+                            
+                            <input type="password" autocomplete="new-password"
+                                className="login-style" name="password" placeholder="Enter Password"
+                                onChange={this.handleChange} />
+                            
+                                {errors.password.length > 0 && 
+                                    <span className='error'>{errors.password}</span>}
+                            
+                            <input type="text" autocomplete="off_randomstring"
+                                className="login-style" name="phone_number"
+                                placeholder="Enter Phone Number (Format should be: +1XXXXXXXXXX)"
+                                onChange={this.handleChange} />
+                            
+                                {errors.phone_number.length > 0 && 
+                                <span className='error'>{errors.phone_number}</span>}
+                            
+                            <br/>
+                            <button className="btn btn-primary"> Submit  </button>
+                        </form>
                     </div>
-                </div> 
-            )
-        }
-  }
+                )
+            } else if (this.state.signedup && !this.state.confirmed) {
+                return (
+                    <div className="register-container">
+                        <img src={registration} height="250px" />
+                        <form onSubmit={this.handlesubmit} class="form-container">
+                            <h5> You should have received the verification code in the e-mail.
+                            Plese check the e-mail and enter the verification code below and click Submit !</h5>
+                            {this.state.hasError && <h5 className="errorStyle"> {this.state.errorDescription}</h5>}
+    
+                            <input type="text" name="confirmation_code" className="login-style" placeholder="Enter Verification Code" onChange={this.handleChange} />
+                            <br />
+                            <button className="btn btn-primary"> Submit  </button>
+                        </form>
+                    </div>
+                )
+            } else if (this.state.signedup && this.state.confirmed) {
+                return (
+                    <div className="register-container">
+                        <img src={registration} height="250px" />
+                        <div className="form-container"> 
+                            <h3> Congratulations !! Registration successfully completed. </h3>
+                            <br />
+                            <button className="btn btn-primary"> Go to Login Page  </button>
+                        </div>
+                    </div> 
+                )
+            }
+      }
 }
 
 export default REgister1;
